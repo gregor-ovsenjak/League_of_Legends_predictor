9996742f30f52442a24c17cb82c70c5b22eb26ce
GregorOvsenjak <gregor.ovsenjak@gmail.com>
2020-06-22 17:51:00 +0200

    Dodal funkcijo v class, ki izriše korelacijsko matriko  10 najbolj koreliranih featurov z target_value.

--- Data_visualization/Feature_selection.py
+++ Data_visualization/Feature_selection.py
@@ -30,14 +30,25 @@ class FeatImp():
         X,y = seperate_target_feature(self.data,target_name)
         self.model.fit(X,y)
         # colors of the bars 
-        my_colors = list(islice(cycle(['b', 'r', 'g', 'gold', 'm','olive','purple','cyan','maroon']), None, len(X)))
+        my_colors = list(islice(cycle(['blue', 'red', 'green', 'gold', 'm','olive','purple','cyan','maroon']), None, len(X)))
         
         # plot graph of feature importances for better visualization
         feat_importances = pd.Series(self.model.feature_importances_, index=X.columns)
         feat_importances.nlargest(10).plot(kind='barh',stacked=True,color = my_colors)
         plt.show()
-
-
-
     
+    def corr_matrix(self,target_name):
+        X,y = seperate_target_feature(self.data,target_name)
+        #get correlations of each features in dataset
+        correlation_matrix = self.data.corr()
+        top_corr_features = correlation_matrix[target_name].nlargest(10).index.values
+        print(top_corr_features)
+        plt.figure(figsize=(10,10))
+        #plot heat map
+        g=sns.heatmap(self.data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+        plt.show()
+
+
+featimp = FeatImp(data = pd.read_csv('../data/lol.csv'))
+featimp.corr_matrix('blueWins')
     
