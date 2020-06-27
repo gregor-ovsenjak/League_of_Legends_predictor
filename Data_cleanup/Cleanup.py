fcd36727cedf6b691c0c75fb5827b7f1826fbc90
GregorOvsenjak <gregor.ovsenjak@gmail.com>
2020-06-27 12:40:17 +0200

    Dodal Pipeline, ki normalizira podatke po Min-Max normalizaciji ali Standardni normalizaciji, lahko pa uporabimo tudi obedve naenkrat. Vrne pa normaliziran DataFrame.

---
+++ Data_cleanup/Cleanup.py
@@ -0,0 +1,87 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+import pandas as pd
+
+
+
+class DataFrameSelector(BaseEstimator,TransformerMixin):
+
+    def __init__(self):
+        None
+    def fit(self,X,y = None):
+        return self
+
+    def transform(self,X,y = None):
+        #print(X[self.attribute_names].values)
+        return X.values
+
+
+def Back_to_DataFrame(X,Sc,MM,Sc_X=None,MM_X=None):
+        columns = X.columns.values
+        if Sc and MM :
+            Sc_df = pd.DataFrame(data = Sc_X,columns = columns)
+            MM_df = pd.DataFrame(data = MM_X,columns = columns)
+            return Sc_df,MM_df
+        elif Sc and not MM :
+            Sc_df = pd.DataFrame(data = Sc_X,columns = columns)
+            return Sc_df
+        elif MM and not Sc :
+            MM_df = pd.DataFrame(data = MM_X,columns = columns)
+            return MM_df
+
+
+
+
+class Normalisation(BaseEstimator,TransformerMixin):
+
+    def __init__(self,Sc = True,MM = False):
+        '''Class za normalizacijo podatkov. Uporabi lahko dve metodi -->Standard Scaling ali pa Min 
+           -Max normalizacijo, lahko pa tudi obedve naenkrat, odvisno od hyperparametrov.
+           Vrne pa transformiran DataFrame.  
+        '''
+        self.Sc = Sc
+        self.MM = MM
+        self.Sc_ = StandardScaler()
+        self.MM_ = MinMaxScaler()
+        self.Std_scaler_pipeline = Pipeline([('dataFS',DataFrameSelector()),
+                                        ('Std_scaler',self.Sc_),
+                                    ])
+        self.Min_Max_pipeline = Pipeline([ ('dataFS_cat',DataFrameSelector()),
+                                       ('Min_Max_scaler', self.MM_),     
+                                    ])
+
+    def transform(self,X,y = None,transform = False):
+        if not transform:
+            if self.Sc and self.MM :
+                Sc_X = self.Std_scaler_pipeline.fit_transform(X)
+                MM_X = self.Min_Max_pipeline.fit_transform(X)
+                Sc_transformed,MM_transformed = Back_to_DataFrame(X,self.Sc,self.MM,Sc_X,MM_X)
+                return Sc_transformed,MM_transformed
+            elif self.Sc and not self.MM :
+                Sc_X = self.Std_scaler_pipeline.fit_transform(X)
+                Sc_transformed= Back_to_DataFrame(X,self.Sc,self.MM,Sc_X)
+                return Sc_transformed
+            elif self.MM and not self.Sc :
+                MM_X = self.Min_Max_pipeline.fit_transform(X)
+                MM_transformed = Back_to_DataFrame(X,self.Sc,self.MM,None,MM_X)
+                return MM_transformed
+        else:
+            if self.Sc and self.MM :
+                Sc_X = self.Std_scaler_pipeline.transform(X)
+                MM_X = self.Min_Max_pipeline.transform(X)
+                Sc_transformed,MM_transformed = Back_to_DataFrame(X,self.Sc,self.MM,Sc_X,MM_X)
+                return Sc_transformed,MM_transformed
+            elif self.Sc and not self.MM :
+                Sc_X = self.Std_scaler_pipeline.transform(X)
+                Sc_transformed= Back_to_DataFrame(X,self.Sc,self.MM,Sc_X)
+                return Sc_transformed
+            elif self.MM and not self.Sc :
+                MM_X = self.Min_Max_pipeline.transform(X)
+                MM_transformed = Back_to_DataFrame(X,self.Sc,self.MM,None,MM_X)
+                return MM_transformed
+
