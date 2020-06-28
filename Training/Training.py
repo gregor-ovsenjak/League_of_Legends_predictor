6ae6c9b3d293d993678694300f853386daa92ee9
GregorOvsenjak <gregor.ovsenjak@gmail.com>
2020-06-28 09:08:17 +0200

    Dodal mapo Training v kateri sem napisal kodo za cross-validacijo nekaterih izbranih ML modelov. Funkcija izriše histogram ter vrne procent uspesnosti ML-algoritma

---
+++ Training/Training.py
@@ -0,0 +1,43 @@
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score, KFold, learning_curve
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+sns.set(style='white', context='notebook', palette='deep')
+
+
+
+
+def cross_val_results(X_train,Y_train):
+    kfold = KFold(n_splits=10)
+    random_state = 2
+    classifiers = []
+    classifiers.append(SVC(random_state=random_state))
+    classifiers.append(AdaBoostClassifier(DecisionTreeClassifier(random_state=random_state),random_state=random_state,learning_rate=0.1))
+    classifiers.append(RandomForestClassifier(random_state=random_state))
+    classifiers.append(ExtraTreesClassifier(random_state=random_state))
+    classifiers.append(KNeighborsClassifier())
+    classifiers.append(LogisticRegression(random_state = random_state))
+
+    cv_results = []
+    for classifier in classifiers :
+        cv_results.append(cross_val_score(classifier, X_train, y = Y_train, scoring = "accuracy", cv = kfold,))
+
+    cv_means = []
+    cv_std = []
+    for cv_result in cv_results:
+        cv_means.append(cv_result.mean())
+        cv_std.append(cv_result.std())
+
+    cv_res = pd.DataFrame({"CrossValMeans":cv_means,"CrossValerrors": cv_std,"Algorithm":["SVC","AdaBoost",
+    "RandomForest","ExtraTrees","KNeighboors","LogisticRegression"]})
+
+    g = sns.barplot("CrossValMeans","Algorithm",data = cv_res, palette="Set3",orient = "h",**{'xerr':cv_std})
+    g.set_xlabel("Mean Accuracy")
+    g = g.set_title("Cross validation scores")
+    return cv_res
